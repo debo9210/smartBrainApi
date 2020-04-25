aa2e935c3534b1293b33eb69c7120c17bc129fa1
debo9210 <omololuadebowale1@gmail.com>
2020-04-25 04:26:17 -0400

    updated response

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send("It's working....");
+  res.send("It's working as expected....");
 });
 
 app.post('/signin', (req, res) => {
