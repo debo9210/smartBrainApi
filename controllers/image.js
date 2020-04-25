ff793ad7b91dc0a5416d1216127b25bd430c20f5
debo9210 <omololuadebowale1@gmail.com>
2020-04-25 03:41:50 -0400

    Initial commit

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+//jshint esversion:10
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '115a407326214bff956108df7297015f',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json('Unable to work with API');
+    });
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json('Unable to get entries'));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall,
+};
---
+++ server.js
@@ -0,0 +1,62 @@
+//jshint esversion:10
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-node');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const knex = require('knex');
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+});
+
+// db.select('*')
+//   .from('users')
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send(dataBase.users);
+});
+
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+const port = process.env.PORT || process.env;
+
+app.listen(port, () => {
+  console.log('Server Started on Port 3001...');
+});
